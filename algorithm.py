56b6f27cbbfd00239cbc9a32173054ebd768b496
liversonp <liverson.p@gmail.com>
2022-07-11 09:36:27 -0300

    commit inicial

---
+++ algorithm.py
@@ -0,0 +1,11 @@
+graph = {"copacabana":{"cristo redentor": 11900, "pao de acucar": 3000, "mudeu do amanha": 13700, "ipanema": 2000, "jardim botanico": 6300, "parque lage": 5600},
+"cristo redentor":{"copacabana": 11900, "pao de acucar": 12300, "mudeu do amanha": 13300, "ipanema": 12800, "jardim botanico": 9500, "parque lage": 8300},
+"pao de acucar":{"copacabana": 3000, "cristo redentor": 12300, "mudeu do amanha": 10600, "ipanema": 5, "jardim botanico": 6, "parque lage": 7},
+"museu do amanha":{"copacabana": 13700, "cristo redentor": 13300, "pao de acucar": 10600, "ipanema": 15900, "jardim botanico": 12600, "parque lage": 11500},
+"ipanema":{"copacabana": 2000, "cristo redentor": 12800, "pao de acucar": 6400, "mudeu do amanha": 15900, "jardim botanico": 4900, "parque lage": 5200},
+"jardim botanico":{"copacabana": 6300, "cristo redentor": 9500, "pao de acucar": 6700, "mudeu do amanha": 12600, "ipanema": 4900, "parque lage": 1400},
+"parque laje":{"copacabana": 5600, "cristo redentor": 8300, "pao de acucar": 5800, "mudeu do amanha": 11500, "ipanema": 5200, "jardim botanico": 1400}}
+
+
+if __name__ == "__main__":
+    print(graph)
